9c71c2b0aedad62b6602c9c220057b0089ce9f3d
Jimmy Rao <32654532+jimjimrao@users.noreply.github.com>
2022-09-13 21:37:09 -0400

    added title, picture, base layout/classes

---
+++ app.js
@@ -0,0 +1,14 @@
+const countdown = () => {
+    const countDate = new Date('March 21, 2023, 00:00:00').getTime()
+    const now = new Date().getTime()
+    const gap = constDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+
+};
+
+countdown();
